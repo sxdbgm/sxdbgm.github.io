10f3d9ac244a240eb9d1b6c348e1681b045c5c5a
sxdbgm <128557615+sxdbgm@users.noreply.github.com>
2023-03-26 14:23:12 +0000

    修改

--- js/index.js
+++ js/index.js
@@ -1,4 +1,24 @@
 new WOW().init();
-$("#slide-view").css({
-    "background-image": "url(" + INDEX_SIDER_BASE64 + ")"
-});
+
+try {
+    $("#slide-view").css({
+        "background-image": "url(" + INDEX_SIDER_BASE64 + ")"
+    });
+} catch (error) {
+   console.log(error); 
+}
+
+try {
+    $("#index-img-1").attr("src", INDEX_1);
+    $("#index-img-2").attr("src", INDEX_2);
+    $("#index-img-3").attr("src", INDEX_3);
+} catch (error) {
+   console.log(error); 
+}
+
+try {
+    $("#service-img-1").attr("src", SERVICE_1);
+    $("#service-img-2").attr("src", SERVICE_2);
+} catch (error) {
+    console.log(error);
+}
